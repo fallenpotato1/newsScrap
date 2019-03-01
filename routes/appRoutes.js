a29804b53db0f2513d59f5367cf85d7aef891714
fallen potato <cruzjon32@gmail.com>
2019-03-01 00:16:36 -0700

    idk

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -3,12 +3,12 @@ var router = express.Router()
 var theData = require("./scraper")
 var gamingItem = require("../models/createLink")
 var mongoose = require("mongoose")
+theData.returnData()
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gamingList";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 router.get("/", function(req, res) {
-    theData.returnData()
     gamingItem.find({}, function(err, docs) {
         res.render("index", {thing: docs})
     })
